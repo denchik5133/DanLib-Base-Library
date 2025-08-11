5144a400638170accdbd7386fd72d723bd663e7a
denchik <100933541+denchik5133@users.noreply.github.com>
2025-08-11 21:12:57 +1000

    Update cl_scrollpanel.lua

--- lua/danlib/core/client/cl_scrollpanel.lua
+++ lua/danlib/core/client/cl_scrollpanel.lua
@@ -17,52 +17,293 @@
  
 
 
---- Creates a vertical scrollbar.
--- @param parent: Parent panel for the scrollbar.
+local DBase = DanLib.Func
+local DCustomUtils = DanLib.CustomUtils.Create
 
-local SCROLL = {}
 
-AccessorFunc(SCROLL, 'pnlCanvas', 'Canvas')
+local SCROLLBAR = DanLib.UiPanel()
 
-function SCROLL:Init()
-    self.canvas = DanLib.CustomUtils.Create(self)
-    self.canvas:SetMouseInputEnabled(true)
-    self.canvas.PerformLayout = function()
-        self:PerformLayoutInternal()
-        self:InvalidateParent()
+AccessorFunc(SCROLLBAR, 'm_HideButtons', 'HideButtons')
+
+function SCROLLBAR:Init()
+    self.Offset = 0
+    self.Scroll = 0
+    self.SmoothScroll = 0
+    self.CanvasSize = 1
+    self.BarSize = 1
+
+    self.btnGrip = DCustomUtils(self, 'DScrollBarGrip')
+    self:SetSize(15, 15)
+    self:SetHideButtons(false)
+end
+
+function SCROLLBAR:SetEnabled(b)
+    if (not b) then
+        self.Offset = 0
+        self:SetScroll(0)
+        self.HasChanged = true
     end
 
-    self.VBar = DanLib.CustomUtils.Create(self, 'DVScrollBar')
-    self.VBar:Pin(RIGHT)
-    self.VBar:SetWide(10)
-    self.VBar:SetHideButtons(true)
+    self:SetMouseInputEnabled(b)
+    self:SetVisible(b)
 
-    self:SetMouseInputEnabled(true)
+    if (self.Enabled ~= b) then
+        self:GetParent():InvalidateLayout()
 
-    local alpha = 0
-    local cornerRadius = 4
-    self.VBar:ApplyClearPaint()
-    self.VBar:ApplyBackground(DanLib.Func:Theme('scroll'), cornerRadius)
-    self.VBar.btnGrip.Paint = function(sl, w, h)
-        alpha = (sl:IsHovered() or sl.Depressed) and math.Clamp(alpha + 5, 0, 100) or math.Clamp(alpha - 5, 0, 100)
-        draw.RoundedBox(cornerRadius, 0, 0, w, h, DanLib.Func:Theme('scroll_dark'))
-        surface.SetAlphaMultiplier(alpha / 255)
-            draw.RoundedBox(cornerRadius, 0, 0, w, h, DanLib.Func:Theme('decor'))
-        surface.SetAlphaMultiplier(1)
+        if self:GetParent().OnScrollbarAppear then
+            self:GetParent():OnScrollbarAppear()
+        end
+    end
+
+    self.Enabled = b
+end
+
+function SCROLLBAR:Value()
+    return self.Pos
+end
+
+function SCROLLBAR:BarScale()
+    if (self.BarSize == 0) then
+        return 1
+    end
+
+    return self.BarSize / (self.CanvasSize + self.BarSize)
+end
+
+function SCROLLBAR:SetUp(_barsize_, _canvassize_)
+    self.BarSize = _barsize_
+    self.CanvasSize = math.max(_canvassize_ - _barsize_, 1)
+    self:SetEnabled(_canvassize_ > _barsize_)
+    self:InvalidateLayout()
+end
+
+function SCROLLBAR:OnMouseWheeled(dlta)
+    if (not self:IsVisible()) then
+        return false
+    end
+
+    if (self.Scroll < 0 or self.Scroll > self.CanvasSize) then
+        return self.SmoothScroll
+    end
+
+    local OldSmoothScroll = self.SmoothScroll
+    local force = 2
+    self.SmoothScroll = math.Clamp(self.SmoothScroll - dlta * force, -self.CanvasSize, self.CanvasSize)
+
+    return OldSmoothScroll ~= self.SmoothScroll
+end
+
+function SCROLLBAR:Think()
+    if (self.SmoothScroll == 0) then
+        return
+    end
+
+    local speed = FrameTime() * 10
+
+    if (self.SmoothScroll > 0) then
+        self.SmoothScroll = math.Clamp(self.SmoothScroll - speed, 0, self.CanvasSize)
+    else
+        self.SmoothScroll = math.Clamp(self.SmoothScroll + speed, -self.CanvasSize, 0)
+    end
+
+    self.Scroll = math.Clamp(self.Scroll + self.SmoothScroll, 0, self.CanvasSize)
+
+    if (self.SmoothScroll > 0) then
+        if (self.Scroll >= self.CanvasSize) then
+            self.SmoothScroll = 0
+        end
+    elseif (self.Scroll <= 0) then
+        self.SmoothScroll = 0
+    end
+
+    self:InvalidateLayout()
+
+    local func = self:GetParent().OnVScroll
+    if func then
+        func(self:GetParent(), self:GetOffset())
+    else
+        self:GetParent():InvalidateLayout()
     end
 end
 
-function SCROLL:SideToggle()
-    self.sideToggle = true
-    self.VBar:Dock(LEFT)
+function SCROLLBAR:AddScroll(dlta)
+    local OldScroll = self:GetScroll()
+    dlta = dlta * 25
+    self:SetScroll(self:GetScroll() + dlta)
+
+    return OldScroll ~= self:GetScroll()
+end
+
+function SCROLLBAR:SetScroll(scrll)
+    if (not self.Enabled) then
+        self.Scroll = 0
+        return
+    end
+
+    self.Scroll = math.Clamp(scrll, 0, self.CanvasSize)
+    self:InvalidateLayout()
+
+    local func = self:GetParent().OnVScroll
+    if func then
+        func(self:GetParent(), self:GetOffset())
+    else
+        self:GetParent():InvalidateLayout()
+    end
+end
+
+function SCROLLBAR:AnimateTo(scrll, length, delay, ease)
+    local anim = self:NewAnimation(length, delay, ease)
+    anim.StartPos = self.Scroll
+    anim.TargetPos = scrll
+    anim.Think = function(anim, pnl, fraction)
+        pnl:SetScroll(Lerp(fraction, anim.StartPos, anim.TargetPos))
+    end
+end
+
+function SCROLLBAR:GetScroll()
+    if (not self.Enabled) then
+        self.Scroll = 0
+    end
+
+    return self.Scroll
+end
+
+function SCROLLBAR:GetOffset()
+    if (not self.Enabled) then
+        return 0
+    end
+
+    return self.Scroll * -1
+end
+
+function SCROLLBAR:Paint(w, h)
+    derma.SkinHook('Paint', 'VScrollBar', self, w, h)
+    return true
+end
+
+function SCROLLBAR:OnMousePressed()
+    local _, y = self:CursorPos()
+    local PageSize = self.BarSize
+    self.SmoothScroll = 0
+
+    if (y > self.btnGrip.y) then
+        self:SetScroll(self:GetScroll() + PageSize)
+    else
+        self:SetScroll(self:GetScroll() - PageSize)
+    end
+end
+
+function SCROLLBAR:OnMouseReleased()
+    self.Dragging = false
+    self.DraggingCanvas = nil
+    self:MouseCapture(false)
+    self.SmoothScroll = 0
+    self.btnGrip.Depressed = false
+end
+
+function SCROLLBAR:OnCursorMoved(x, y)
+    if (not self.Enabled) then
+        return
+    end
+
+    if (not self.Dragging) then
+        return
+    end
+
+    local _, y = self:ScreenToLocal(0, gui.MouseY())
+    y = y - self.HoldPos
+
+    local TrackSize = self:GetTall() - self.btnGrip:GetTall()
+    y = y / TrackSize
+    self.SmoothScroll = 0
+    self:SetScroll(y * self.CanvasSize)
+end
+
+function SCROLLBAR:Grip()
+    if (not self.Enabled) then
+        return
+    end
+
+    if (self.BarSize == 0) then
+        return
+    end
+
+    self:MouseCapture(true)
+    self.Dragging = true
+    local _, y = self.btnGrip:ScreenToLocal(0, gui.MouseY())
+    self.HoldPos = y
+    self.btnGrip.Depressed = true
+end
+
+function SCROLLBAR:PerformLayout()
+    local _wide = self:GetWide()
+    local _scroll = self:GetScroll() / self.CanvasSize
+    local _barSize = math.max(self:BarScale() * self:GetTall(), 10)
+    local _track = self:GetTall() - _barSize
+    _track = _track + 1
+    _scroll = _scroll * _track
+    self.btnGrip:SetPos(0, _scroll)
+    self.btnGrip:SetSize(_wide, _barSize)
+end
+
+SCROLLBAR:Register('DanLib.UI.ScrollBar')
+
+
+
+
+
+local SCROLL = DanLib.UiPanel()
+AccessorFunc(SCROLL, 'Padding', 'Padding')
+AccessorFunc(SCROLL, 'pnlCanvas', 'Canvas')
+
+function SCROLL:Init()
+    self.pnlCanvas = DCustomUtils(self)
+    self.pnlCanvas:ApplyEvent('OnMousePressed', function(sl, code)
+        sl:GetParent():OnMousePressed(code)
+    end)
+    self.pnlCanvas:SetMouseInputEnabled(true)
+
+    self.pnlCanvas.PerformLayout = function(pnl)
+        self:PerformLayout()
+        self:InvalidateParent()
+    end
+
+    self.VBar = DCustomUtils(self, 'DanLib.UI.ScrollBar')
+    self.VBar:SetWide(10)
+    self.VBar:Dock(RIGHT)
+    self:SetPadding(0)
+    self:SetMouseInputEnabled(true)
+    -- This turns off the engine drawing
+    self:SetPaintBackgroundEnabled(false)
+    self:SetPaintBorderEnabled(false)
+    self:SetPaintBackground(false)
+
+    local VBar = self:GetVBar()
+    local alpha = 0
+    local cornerRadius = 6
+    VBar:CustomUtils()
+    VBar:ApplyClearPaint()
+    VBar.btnGrip.Paint = function(sl, w, h)
+        alpha = (sl:IsHovered() or sl.Depressed) and math.Clamp(alpha + 5, 0, 100) or math.Clamp(alpha - 5, 0, 100)
+        draw.RoundedBox(cornerRadius, 0, 0, w, h, DBase:Theme('scroll_dark'))
+        surface.SetAlphaMultiplier(alpha / 255)
+            draw.RoundedBox(cornerRadius, 0, 0, w, h, DBase:Theme('decor'))
+        surface.SetAlphaMultiplier(1)
+        self.PaintBackground = true
+    end
+    VBar.btnGrip:SetCursor('hand')
+    VBar:SetCursor('hand')
+
+    self:SetWide(self:GetWide() * 0.75)
+    VBar:SetWide(VBar:GetWide() * 0.75)
 end
 
 function SCROLL:ToggleScrollBar()
     self:GetVBar():SetWide(0)
 end
 
-function SCROLL:AddItem(parent)
-    parent:SetParent(self:GetCanvas())
+function SCROLL:AddItem(pnl)
+    pnl:SetParent(self:GetCanvas())
 end
 
 function SCROLL:OnChildAdded(child)
@@ -70,7 +311,7 @@ function SCROLL:OnChildAdded(child)
 end
 
 function SCROLL:SizeToContents()
-    self:SetSize(self.canvas:GetSize())
+    self:SetSize(self.pnlCanvas:GetSize())
 end
 
 function SCROLL:GetVBar()
@@ -78,7 +319,7 @@ function SCROLL:GetVBar()
 end
 
 function SCROLL:GetCanvas()
-    return self.canvas
+    return self.pnlCanvas
 end
 
 function SCROLL:InnerWidth()
@@ -87,71 +328,121 @@ end
 
 function SCROLL:Rebuild()
     self:GetCanvas():SizeToChildren(false, true)
+
+    -- Although this behaviour isn't exactly implied, center vertically too
     if (self.m_bNoSizing and self:GetCanvas():GetTall() < self:GetTall()) then
         self:GetCanvas():SetPos(0, (self:GetTall() - self:GetCanvas():GetTall()) * 0.5)
     end
 end
 
-function SCROLL:OnMouseWheeled(delta)
-    return self.VBar:OnMouseWheeled(delta)
+function SCROLL:OnMouseWheeled(dlta)
+    return self.VBar:OnMouseWheeled(dlta)
 end
 
 function SCROLL:OnVScroll(iOffset)
-    self.canvas:SetPos(0, iOffset)
+    self.pnlCanvas:SetPos(0, iOffset)
 end
 
 function SCROLL:ScrollToChild(panel)
-    self:InvalidateLayout(true)
-    local x, y = self.canvas:GetChildPosition(panel)
+    self:PerformLayout()
+    local x, y = self.pnlCanvas:GetChildPosition(panel)
     local w, h = panel:GetSize()
-    y = y + h * 0.5 - self:GetTall() * 0.5
+    y = y + h * 0.5
+    y = y - self:GetTall() * 0.5
     self.VBar:AnimateTo(y, 0.5, 0, 0.5)
 end
 
-function SCROLL:PerformLayoutInternal()
-    local Tall = self.canvas:GetTall()
-    local Wide = self:GetWide()
-    local YPos = 0
-
+function SCROLL:PerformLayout()
+    local _tall = self.pnlCanvas:GetTall()
+    local _wide = self:GetWide()
+    local y_pos = 0
     self:Rebuild()
+    self.VBar:SetUp(self:GetTall(), self.pnlCanvas:GetTall())
+    y_pos = self.VBar:GetOffset()
 
-    self.VBar:SetUp(self:GetTall(), self.canvas:GetTall())
-    YPos = self.VBar:GetOffset()
-
-    if self.VBar.Enabled then
-        Wide = Wide - self.VBar:GetWide()
+    if (self.VBar.Enabled) then
+        _wide = _wide - self.VBar:GetWide()
     end
 
-    self.canvas:SetPos(self.sideToggle and 18 or 0, YPos)
-    self.canvas:SetWide(self.sideToggle and Wide or Wide - 4)
-
+    self.pnlCanvas:SetPos(0, y_pos)
+    self.pnlCanvas:SetWide(_wide)
     self:Rebuild()
 
-    if (Tall ~= self.canvas:GetTall()) then
+    if (_tall ~= self.pnlCanvas:GetTall()) then
+        self.VBar:SetScroll(self.VBar:GetScroll()) -- Make sure we are not too far down!
+    end
+end
+
+function SCROLL:Clear()
+    return self.pnlCanvas:Clear()
+end
+
+local OldPerformLayout = function(self)
+    local _tall = self.pnlCanvas:GetTall()
+    local _wide = self:GetWide()
+    local y_pos = 0
+    self:Rebuild()
+    self.VBar:SetUp(self:GetTall(), self.pnlCanvas:GetTall())
+    y_pos = self.VBar:GetOffset()
+
+    if (self.VBar.Enabled) then
+        _wide = _wide - self.VBar:GetWide()
+    end
+
+    self.pnlCanvas:SetPos(0, y_pos)
+    self.pnlCanvas:SetWide(_wide)
+    self:Rebuild()
+
+    if (_tall ~= self.pnlCanvas:GetTall()) then
+        self.VBar:SetScroll(self.VBar:GetScroll()) -- Make sure we are not too far down!
+    end
+end
+
+function SCROLL:NewPerformLayout()
+    local _tall = self.pnlCanvas:GetTall()
+    local _wide = self:GetWide()
+    local y_pos = 0
+    self:Rebuild()
+    self.VBar:SetUp(self:GetTall(), self.pnlCanvas:GetTall())
+    y_pos = self.VBar:GetOffset()
+
+    if self.VBar.Enabled then
+        _wide = _wide - self.VBar:GetWide()
+    end
+
+    self.pnlCanvas:SetPos(self.VBar:GetWide(), y_pos)
+    self.pnlCanvas:SetWide(_wide)
+    self:Rebuild()
+
+    if _tall ~= self.pnlCanvas:GetTall() then
         self.VBar:SetScroll(self.VBar:GetScroll())
     end
 end
 
-function SCROLL:PerformLayout()
-    self:PerformLayoutInternal()
+function SCROLL:OldOnVScroll(iOffset)
+    self.pnlCanvas:SetPos(0, iOffset)
 end
 
-function SCROLL:Clear()
-    return self.canvas:Clear()
+function SCROLL:NewOnVScroll(iOffset)
+    self.pnlCanvas:SetPos(self.VBar:GetWide(), iOffset)
 end
 
-function SCROLL:Paint()
-
+function SCROLL:SetLeft(b)
+    local VBar = self:GetVBar()
+    VBar:Dock(b and LEFT or RIGHT)
+    self.PerformLayout = b and NewPerformLayout or OldPerformLayout
+    self.OnVScroll = b and NewOnVScroll or OldOnVScroll
 end
 
-derma.DefineControl('DanLib.UI.Scroll', '', SCROLL, 'DPanel')
+SCROLL:SetBase('DPanel')
+SCROLL:Register('DanLib.UI.Scroll')
 
 
 
 
 
 
-local CANVAS, _ = DanLib.UiPanel()
+local CANVAS = DanLib.UiPanel()
 
 AccessorFunc(CANVAS, 'm_iSpace', 'Space')
 
@@ -219,26 +510,26 @@ CANVAS:Register('DanLib.UI.ScrollCanvas')
 
 
 --- Horizontal scroll bar
-local HORIZONTALSCROLL, _ = DanLib.UiPanel()
+local HORIZONTALSCROLL = DanLib.UiPanel()
 
 function HORIZONTALSCROLL:Init()
     local alpha = 0
-    local cornerRadius = 4
+    local cornerRadius = 6
 
-    self.scrollBar = DanLib.CustomUtils.Create(self)
+    self.scrollBar = DCustomUtils(self)
     self.scrollBar:SetTall(8)
-    self.scrollBar:ApplyBackground(DanLib.Func:Theme('scroll'), cornerRadius)
+    self.scrollBar:ApplyBackground(DBase:Theme('scroll'), cornerRadius)
 
-    self.scrollBar.grip = DanLib.CustomUtils.Create(self.scrollBar, 'DButton')
+    self.scrollBar.grip = DCustomUtils(self.scrollBar, 'DButton')
     self.scrollBar.grip:SetText('')
     self.scrollBar.grip:SetPos(0, 0)
     self.scrollBar.grip:SetSize(0, self.scrollBar:GetTall())
     self.scrollBar.grip:ApplyClearPaint()
     self.scrollBar.grip:ApplyEvent(nil, function(sl, w, h)
         alpha = (sl:IsHovered() or sl.Depressed) and math.Clamp(alpha + 5, 0, 100) or math.Clamp(alpha - 5, 0, 100)
-        draw.RoundedBox(cornerRadius, 0, 0, w, h, DanLib.Func:Theme('scroll_dark'))
+        draw.RoundedBox(cornerRadius, 0, 0, w, h, DBase:Theme('scroll_dark'))
         surface.SetAlphaMultiplier(alpha / 255)
-            draw.RoundedBox(cornerRadius, 0, 0, w, h, DanLib.Func:Theme('decor'))
+            draw.RoundedBox(cornerRadius, 0, 0, w, h, DBase:Theme('decor'))
         surface.SetAlphaMultiplier(1)
     end)
     self.scrollBar.grip:ApplyEvent('SetScrollX', function(sl, x)
@@ -263,7 +554,7 @@ function HORIZONTALSCROLL:Init()
         self:SetScroll((sl.startX + gui.MouseX() - sl.mouseStartX) / (self:GetWide() - sl:GetWide()))
     end)
 
-    self.canvas = DanLib.CustomUtils.Create(self)
+    self.canvas = DCustomUtils(self)
     self.canvas:SetPos(0, 0)
     self.canvas:SetSize(0, self:GetTall())
     self.canvas:SetZPos(-100)
@@ -337,7 +628,7 @@ function HORIZONTALSCROLL:ScrollToChild(child)
 
     -- Ignore elements with X coordinates equal to 0
     if childX == 0 then
-        -- print("Ignoring child at position 0")
+        -- print('Ignoring child at position 0')
         return
     end
 
@@ -359,7 +650,7 @@ function HORIZONTALSCROLL:ScrollToChild(child)
     -- print('Target X:', targetX)
 
     -- If targetX is defined, scroll
-    if targetX and type(targetX) == "number" then
+    if targetX and type(targetX) == 'number' then
         local maxX = self:GetWide() - self.canvas:GetWide()
         targetX = math.Clamp(targetX, maxX, 0)
         -- print('Scrolling to:', targetX)
@@ -384,7 +675,7 @@ function HORIZONTALSCROLL:AnimateScrollTo(targetX)
 
         if (currentX ~= targetX) then
             -- If you haven't reached the target yet, continue the animation
-            DanLib.Func:TimerSimple(0.01, scrollStep) -- Start the next step after 10 ms
+            DBase:TimerSimple(0.01, scrollStep) -- Start the next step after 10 ms
         else
             -- print('Reached target X:', targetX) -- Debugging information
         end
