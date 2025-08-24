4d429af5f01d41702e37a5a16d143709535dce1b
denchik <100933541+denchik5133@users.noreply.github.com>
2025-08-24 13:59:21 +1000

    Update cl_user_setup.lua

--- lua/danlib/core/pages/cl_user_setup.lua
+++ lua/danlib/core/pages/cl_user_setup.lua
@@ -17,18 +17,18 @@
 
 
 
-local base = DanLib.Func
-local utils = DanLib.Utils
-local Table = DanLib.Table
-local network = DanLib.Network
-local customUtils = DanLib.CustomUtils
-local dConfig = DanLib.Config
+local DBase = DanLib.Func
+local DUtils = DanLib.Utils
+local DTable = DanLib.Table
+local DNetwork = DanLib.Network
+local DCustomUtils = DanLib.CustomUtils.Create
+local DMaterials = DanLib.Config.Materials
 
 local string = string
 local table = table
 local count = table.Count
 
-local SETUP = base.CreatePage('User')
+local SETUP = DBase.CreatePage('User')
 SETUP:SetOrder(2)
 SETUP:SetIcon('bQldJcj')
 SETUP:SetKeyboardInput(true)
@@ -58,7 +58,7 @@ function SETUP:Init()
     self.defaultFont = 'danlib_font_18' -- Default font
 
 
-    self.scroll = customUtils.Create(self, 'DanLib.UI.Scroll')
+    self.scroll = DCustomUtils(self, 'DanLib.UI.Scroll')
     self.scroll:Pin()
 
     self:Refresh() -- Interface update
@@ -67,7 +67,7 @@ end
 
 --- Creates the top header of the panel
 function SETUP:TopHeader()
-    self.header = customUtils.Create(self)
+    self.header = DCustomUtils(self)
     self.header:Pin(TOP)
     self.header:DockMargin(0, 0, 0, 12)
     self.header:SetTall(46)
@@ -75,9 +75,9 @@ function SETUP:TopHeader()
     self.header.icon = 24
     self.header.iconMargin = 14
     self.header:ApplyEvent(nil, function(sl, w, h)
-        utils:DrawRoundedBox(0, 0, w, h, base:Theme('secondary_dark'))
-        utils:DrawIcon(sl.iconMargin, h * .5 - sl.icon * 0.5, sl.icon, sl.icon, 'bQldJcj', base:Theme('mat', 150))
-        utils:DrawDualText(sl.iconMargin * 3.5, h / 2 - 2, base:L('#settings'), 'danlib_font_20', base:Theme('decor'), base:L('#settings.description'), self.defaultFont, base:Theme('text'), TEXT_ALIGN_LEFT, nil, w - 300)
+        DUtils:DrawRoundedBox(0, 0, w, h, DBase:Theme('secondary_dark'))
+        DUtils:DrawIcon(sl.iconMargin, h * .5 - sl.icon * 0.5, sl.icon, sl.icon, 'bQldJcj', DBase:Theme('mat', 150))
+        DUtils:DrawDualText(sl.iconMargin * 3.5, h / 2 - 2, DBase:L('#settings'), 'danlib_font_20', DBase:Theme('decor'), DBase:L('#settings.description'), self.defaultFont, DBase:Theme('text'), TEXT_ALIGN_LEFT, nil, w - 300)
     end)
 
     -- Defining the buttons for saving and cancelling
@@ -102,8 +102,8 @@ end
 -- @param onClick: function The function to call when the button is clicked
 -- @return DButton: The created button
 function SETUP:CreateButton(name, onClick)
-    local buttonSize = utils:TextSize(name, self.defaultFont).w
-    local Button = base.CreateUIButton(self.header, {
+    local buttonSize = DUtils:TextSize(name, self.defaultFont).w
+    local Button = DBase.CreateUIButton(self.header, {
         background = { nil },
         dock_indent = { RIGHT, nil, 7, 6, 7 },
         wide = 14 + buttonSize,
@@ -120,9 +120,9 @@ end
 function SETUP:GetSortedConfig()
     local sortedConfig = {}
     for k, v in pairs(DanLib.UserModules) do
-        Table:Add(sortedConfig, { v.SortOrder, k })
+        DTable:Add(sortedConfig, { v.SortOrder, k })
     end
-    Table:SortByMember(sortedConfig, 1, true) -- Sort by order
+    DTable:SortByMember(sortedConfig, 1, true) -- Sort by order
     return sortedConfig
 end
 
@@ -137,10 +137,10 @@ function SETUP:Refresh()
         local moduleKey = config[2]
         local module = DanLib.UserModules[moduleKey]
 
-    	local iconSize, borderW, borderH, Margin15 = 18, base:Scale(4), 35, base:Scale(10)
+    	local iconSize, borderW, borderH, Margin15 = 18, DBase:Scale(4), 35, DBase:Scale(10)
     	local colorExpanded = module.Color or color_white
 
-        local category = customUtils.Create(self.scroll, 'DCollapsibleCategory')
+        local category = DCustomUtils(self.scroll, 'DCollapsibleCategory')
         category:SetLabel('')
         category:PinMargin(TOP, nil, nil, nil, 10)
         category:ApplyClearPaint()
@@ -152,12 +152,12 @@ function SETUP:Refresh()
         	local tomColor = Expanded and colorExpanded or ColorAlpha(colorExpanded, 100)
         	sl.deg = Lerp(FrameTime() * 15, sl.deg or 0, Expanded and 0 or 180)
 
-            utils:DrawRoundedBox(0, 0, w, h, base:Theme('secondary'))
-            utils:DrawRoundedMask(6, 0, 0, w, h, function()
-            	utils:DrawRoundedBox(0, 0, 3, h, tomColor)
+            DUtils:DrawRoundedBox(0, 0, w, h, DBase:Theme('secondary'))
+            DUtils:DrawRoundedMask(6, 0, 0, w, h, function()
+            	DUtils:DrawRoundedBox(0, 0, 3, h, tomColor)
             end)
 
-            utils:DrawIconRotated(w - 20, h / 2, iconSize, iconSize, sl.deg, dConfig.Materials['Arrow'], tomColor)
+            DUtils:DrawIconRotated(w - 20, h / 2, iconSize, iconSize, sl.deg, DMaterials['Arrow'], tomColor)
             draw.SimpleText(module.Title or 'Untitled', 'danlib_font_18', 14, h / 2, tomColor, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
         end)
 
@@ -183,14 +183,43 @@ function SETUP:CreateSettingRow(parent, module, val, Key, onChange, onReset)
     local headerH = 50
     local customElement = val.Type == DanLib.Type.Table and val.VguiElement
 
-    local variablePanel = customUtils.Create(parent)
+    local variablePanel = DCustomUtils(parent)
     variablePanel:Pin(TOP)
     variablePanel:SetTall(headerH)
     variablePanel:PinMargin(TOP, 2, 8, 4)
     variablePanel:ApplyEvent(nil, function(sl, w, h)
-        utils:DrawRoundedBox(0, 0, w, h, base:Theme('secondary', 150))
-        utils:DrawDualText(10, headerH / 2 - 2, val.Name or nil, self.defaultFont, base:Theme('decor'), val.Description or nil, self.defaultFont, base:Theme('text'), TEXT_ALIGN_LEFT, nil, w - 210)
+        DUtils:DrawRoundedBox(0, 0, w, h, DBase:Theme('secondary', 150))
+
+        -- The size of the input element by type
+        local inputWidth = 300
+        if (val.Type == DanLib.Type.Int or val.Type == DanLib.Type.String) then
+            inputWidth = 270
+        elseif (val.Type == DanLib.Type.Bool) then
+            inputWidth = 105
+        end
+
+        -- Size of the action button (if any)
+        local actionButtonWidth = 0
+        if val.Action then
+            actionButtonWidth = 32 + 10 -- 32px button + 10px indentation
+        end
+
+        -- The total width of the text is subtracted from the total width of the input field, the button, and the margins.
+        local margin = w - inputWidth - actionButtonWidth
+        DUtils:DrawDualText(10, headerH / 2 - 2, val.Name or nil, self.defaultFont, DBase:Theme('decor'), val.Description or nil, self.defaultFont, DBase:Theme('text'), TEXT_ALIGN_LEFT, nil, margin)
     end)
+
+    -- Add help text tooltip functionality if HelpText is provided
+    if val.HelpText then
+        local x = DUtils:TextSize(val.Name, self.defaultFont).w
+        local HelpPanel = DCustomUtils(variablePanel)
+        HelpPanel:SetPos(x + 14, 6)
+        HelpPanel:SetSize(14, 14)
+        HelpPanel:ApplyTooltip(val.HelpText, nil, nil, TOP)
+        HelpPanel:ApplyEvent(nil, function(sl, w, h)
+            DUtils:DrawIcon(0, 0, w, h, DMaterials['Help'] or DMaterials['Info'], DBase:Theme('mat', 100))
+        end)
+    end
 
     self:CreateResetButton(variablePanel, onReset)
     local wide, margiMoveToRight, margin = 200, 15, (variablePanel:GetTall() - 30) * 0.5 -- indentation
@@ -199,7 +228,7 @@ function SETUP:CreateSettingRow(parent, module, val, Key, onChange, onReset)
     if (customElement or val.GetOptions) then
         if val.GetOptions then
             local options = val.GetOptions()
-            local comboSelect = base.CreateUIComboBox(variablePanel)
+            local comboSelect = DBase.CreateUIComboBox(variablePanel)
             comboSelect:PinMargin(RIGHT, nil, margin, margiMoveToRight, margin)
             comboSelect:SetWide(wide)
             comboSelect:SetValue(module:GetValue(val.Key))
@@ -212,24 +241,27 @@ function SETUP:CreateSettingRow(parent, module, val, Key, onChange, onReset)
                 onChange(value)
             end)
         else
-            local button = base.CreateUIButton(variablePanel, {
+            local button = DBase.CreateUIButton(variablePanel, {
                 dock_indent = { RIGHT, nil, margin, margiMoveToRight, margin },
                 wide = 32,
-                icon = { dConfig.Materials['Edit'] },
-                tooltip = { base:L('#edit'), nil, nil, TOP },
+                icon = { DMaterials['Edit'] },
+                tooltip = { DBase:L('#edit'), nil, nil, TOP },
                 click = function(sl)
-                    if ui:valid(sl.con) then sl.con:Remove() return end
-                    local Container = customUtils.Create(nil, val.VguiElement)
+                    if ui:valid(sl.con) then
+                        sl.con:Remove()
+                        return
+                    end
+                    local Container = DCustomUtils(nil, val.VguiElement)
                     sl.con = Container
                     Container:FillPanel()
                 end
             })
         end
     elseif (val.Type == DanLib.Type.Int) then
-        local numberWang = base.CreateNumberWang(variablePanel)
+        local numberWang = DBase.CreateNumberWang(variablePanel)
         numberWang:PinMargin(RIGHT, nil, margin, margiMoveToRight, margin)
         numberWang:SetWide(wide)
-        numberWang:SetHighlightColor(base:Theme('secondary', 50))
+        numberWang:SetHighlightColor(DBase:Theme('secondary', 50))
         numberWang:SetValue(module:GetValue(val.Key))
         numberWang:DisableShadows()
 
@@ -242,17 +274,17 @@ function SETUP:CreateSettingRow(parent, module, val, Key, onChange, onReset)
             onChange(sl:GetValue())
         end)
     elseif (val.Type == DanLib.Type.String) then
-        local textEntry = base.CreateTextEntry(variablePanel)
+        local textEntry = DBase.CreateTextEntry(variablePanel)
         textEntry:PinMargin(RIGHT, nil, margin, margiMoveToRight, margin)
         textEntry:SetWide(wide)
-        textEntry:SetHighlightColor(base:Theme('secondary', 50))
+        textEntry:SetHighlightColor(DBase:Theme('secondary', 50))
         textEntry:SetValue(module:GetValue(val.Key) or '')
         textEntry:DisableShadows()
         textEntry:ApplyEvent('OnChange', function(sl)
             onChange(sl:GetValue())
         end)
     elseif (val.Type == DanLib.Type.Bool) then
-        local CheckBox = base.CreateCheckbox(variablePanel)
+        local CheckBox = DBase.CreateCheckbox(variablePanel)
         CheckBox:PinMargin(RIGHT, nil, margin, margiMoveToRight, margin)
         CheckBox:SetWide(32)
         CheckBox:SetValue(module:GetValue(val.Key))
@@ -261,18 +293,18 @@ function SETUP:CreateSettingRow(parent, module, val, Key, onChange, onReset)
             onChange(value)
         end)
     elseif (val.Type == DanLib.Type.Key) then
-        local binder = base.CreateUIBinder(variablePanel)
+        local binder = DBase.CreateUIBinder(variablePanel)
         binder:PinMargin(RIGHT, nil, margin, margiMoveToRight, margin)
         binder:Help()
         binder:SetWide(wide)
         binder:DisableShadows()
         local dbanedBind = val.bannedKeys or {}
-        local gvalBind = base:ProcessBind(module:GetValue(val.Key), dbanedBind)
+        local gvalBind = DBase:ProcessBind(module:GetValue(val.Key), dbanedBind)
         binder:SetValue(gvalBind)
         function binder:OnChange(value)
-            local valBind = base:ProcessBind(value, dbanedBind)
+            local valBind = DBase:ProcessBind(value, dbanedBind)
             if (valBind == 'NONE') then
-                base:ScreenNotification(base:L('#key.bind.forbidden'), base:L('#key.bind.binding'), 'ERROR')
+                DBase:ScreenNotification(DBase:L('#key.bind.forbidden'), DBase:L('#key.bind.binding'), 'ERROR')
                 binder:SetValue(gvalBind) -- Return the previous value if the new value is invalid
             else
                 onChange(valBind) -- If binding is allowed, call onChange
@@ -293,16 +325,16 @@ end
 -- @param variablePanel Panel: The variable panel to add the action button to
 -- @param val table: The variable configuration
 function SETUP:CreateActionButton(variablePanel, val)
-    local button = base.CreateUIButton(variablePanel, {
+    local button = DBase.CreateUIButton(variablePanel, {
         dock_indent = { RIGHT, nil, 10, 15 - 4, 10 },
         wide = 32,
-        icon = { DanLib.Config.Materials['Info'] },
+        icon = { DMaterials['Info'] },
         tooltip = {'Read More', nil, nil, TOP },
         click = function()
             if (type(val.Action) == 'function') then
                 val.Action()  -- If it is a function, call it
             elseif (type(val.Action) == 'string') then
-                customUtils.Create(nil, val.Action)  -- Register an item with vgui
+                DCustomUtils(nil, val.Action)  -- Register an item with vgui
             end
         end
     })
@@ -310,11 +342,11 @@ end
 
 
 function SETUP:CreateResetButton(variablePanel, onReset)
-    local button = base.CreateUIButton(variablePanel, {
+    local button = DBase.CreateUIButton(variablePanel, {
         dock_indent = { RIGHT, nil, 10, 15 - 4, 10 },
         wide = 32,
-        icon = { DanLib.Config.Materials['Reset'] },
-        tooltip = { base:L('#resetting.changes'), nil, nil, TOP },
+        icon = { DMaterials['Reset'] },
+        tooltip = { DBase:L('#resetting.changes'), nil, nil, TOP },
         click = function()
             onReset()
             self:Refresh()
