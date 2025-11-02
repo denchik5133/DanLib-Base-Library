d4d70cf14c44097a2291700ace3e597e66608fb8
denchik <denchik5133@gmail.com>
2025-11-02 16:14:16 +1000

    Update cl_dcombobox.lua

--- lua/danlib/core/client/cl_dcombobox.lua
+++ lua/danlib/core/client/cl_dcombobox.lua
@@ -1,65 +1,84 @@
 /***
- *   @addon         DanLib
- *   @version       3.0.0
- *   @release_date  10/4/2023
+ *   @component     DanLib ComboBox
+ *   @version       1.4.0
+ *   @file          cl_dcombobox.lua
  *   @author        denchik
  *   @contact       Discord: denchik_gm
  *                  Steam: https://steamcommunity.com/profiles/76561198405398290/
  *                  GitHub: https://github.com/denchik5133
  *                
- *   @description   Universal library for GMod Lua, combining all the necessary features to simplify script development. 
- *                  Avoid code duplication and speed up the creation process with this powerful and convenient library.
+ *   @description   Customizable ComboBox (dropdown) component for DanLib UI framework.
+ *                  Provides a dropdown selection menu with support for icons, custom fonts,
+ *                  sorting, and smooth animations. Only one menu can be open at a time.
  *
- *   @usage         !danlibmenu (chat) | danlibmenu (console)
+ *   @part_of       DanLib v3.0.0 and higher
+ *                  https://github.com/denchik5133/danlib
+ *
+ *   @features      - Single active menu enforcement (closes other ComboBoxes)
+ *                  - Customizable text direction and alignment
+ *                  - Icon support for options
+ *                  - Smooth rotation animation for dropdown indicator
+ *                  - Sortable options
+ *                  - Custom font support
+ *
+ *   @usage         local combo = DBase.CreateUIComboBox(parent)
+ *                  combo:AddChoice('Option 1', 1, true)
+ *                  combo:SetDirection(true)
+ *                  combo:SetFont('danlib_font_18')
+ *
  *   @license       MIT License
- *   @notes         For feature requests or contributions, please open an issue on GitHub.
+ *   @notes         Requires DanLib framework to function properly.
  */
 
 
 /***
- *   cl_dcombobox.lua
- *   This file is responsible for creating a customisable combobox in a DanLib project.
- *
- *   It includes the following functions:
- *   - Initialisation of the combo box with default settings.
- *   - Adding options with text, data and icons.
- *   - Selects an option and retrieves the selected option.
- *   - Opening and closing a drop-down menu.
- *   - Handling changes in the values of constants (ConVar).
- *   - Adjusting the background colour and selection.
- *   - Drawing a stateful combobox (normal, induced, open).
- *
- *   The file provides a convenient interface for creating comboboxes in a project.
+ *   Internal Implementation
+ *   Handles dropdown menu behavior, option management, and rendering.
  */
 
 
 
-local base = DanLib.Func
-local Table = DanLib.Table
-local utils = DanLib.Utils
-local CustomUtils = DanLib.CustomUtils
-local PANEL = {}
+local DBase = DanLib.Func
+local DTable = DanLib.Table
+local DUtils = DanLib.Utils
+local DCustomUtils = DanLib.CustomUtils.Create
 
-Derma_Hook(PANEL, 'Paint', 'Paint', 'ComboBox')
+-- Cached global functions for performance
+local _IsValid = IsValid
+local _pairs = pairs
+local _max = math.max
+local _min = math.min
+local _Lerp = Lerp
+local _stringlen = string.len
+local _tostring = tostring
+local _isnumber = isnumber
+local _tonumber = tonumber
+local _FrameTime = FrameTime
+local _SimpleText = draw.SimpleText
 
-Derma_Install_Convar_Functions(PANEL)
+-- Global tracker for currently open ComboBox (ensures only one menu is open at a time)
+local ActiveComboBox = nil
 
-AccessorFunc(PANEL, 'm_bDoSort', 'SortItems', FORCE_BOOL)
+local COMBOBOX = DanLib.UiPanel()
 
-function PANEL:Init()
-    -- Setup internals
+-- Defining an accessor for sorting items
+COMBOBOX:Accessor('SortItems', COMBOBOX.Boolean, {
+    default = true
+})
+
+--- Initializes the ComboBox with default settings.
+function COMBOBOX:Init()
     self:SetTall(22)
     self:Clear()
-
     self:SetText('')
     self:SetContentAlignment(4)
     self:SetTextInset(8, 0)
     self:SetIsMenu(true)
-    self:SetSortItems(true)
     self.iconAngle = 0
 end
 
-function PANEL:Clear()
+--- Clears all choices and resets the ComboBox to default state.
+function COMBOBOX:Clear()
     self:SetText('')
     self.Choices = {}
     self.Data = {}
@@ -69,145 +88,191 @@ function PANEL:Clear()
     self:CloseMenu()
 end
 
-function PANEL:GetOptionText(index)
+--- Gets the display text for an option by index.
+-- @param index (number): The index of the option.
+-- @return (string): The display text of the option.
+function COMBOBOX:GetOptionText(index)
     return self.Choices[index]
 end
 
-function PANEL:GetOptionData(index)
+--- Gets the associated data for an option by index.
+-- @param index (number): The index of the option.
+-- @return (any): The data associated with the option.
+function COMBOBOX:GetOptionData(index)
     return self.Data[index]
 end
 
-function PANEL:GetOptionTextByData(data)
-    for id, dat in pairs(self.Data) do
-        if (dat == data) then
-            return self:GetOptionText(id)
-        end
-    end
-
-    -- Try interpreting it as a number
-    for id, dat in pairs(self.Data) do
-        if (dat == tonumber(data)) then
-            return self:GetOptionText(id)
-        end
-    end
-
-    -- In case we fail
-    return data
-end
-
-function PANEL:PerformLayout(w, h)
-    -- Make sure the text color is updated
+--- Handles layout updates.
+-- @param w (number): Width of the panel.
+-- @param h (number): Height of the panel.
+function COMBOBOX:PerformLayout(w, h)
     DButton.PerformLayout(self, w, h)
 end
 
-function PANEL:ChooseOption(value, index)
+--- Selects an option by value and index.
+-- @param value (string): The display text of the option.
+-- @param index (number): The index of the option.
+function COMBOBOX:ChooseOption(value, index)
     self:CloseMenu()
     self.text = value
     self.selected = index
     self:OnSelect(index, value, self.Data[index])
 end
 
-function PANEL:ChooseOptionID(index)
+--- Selects an option by its index.
+-- @param index (number): The index of the option to select.
+function COMBOBOX:ChooseOptionID(index)
     local value = self:GetOptionText(index)
     self:ChooseOption(value, index)
 end
 
-function PANEL:GetSelectedID()
+--- Gets the index of the currently selected option.
+-- @return (number): The index of the selected option, or nil if none selected.
+function COMBOBOX:GetSelectedID()
     return self.selected
 end
 
-function PANEL:GetSelected()
-    if (not self.selected) then return end
+--- Gets the text and data of the currently selected option.
+-- @return (string, any): The display text and associated data, or nil if none selected.
+function COMBOBOX:GetSelected()
+    if not self.selected then
+        return
+    end
     return self:GetOptionText(self.selected), self:GetOptionData(self.selected)
 end
 
-function PANEL:OnSelect(index, value, data)
+--- Called when an option is selected. Override this for custom behavior.
+-- @param index (number): The index of the selected option.
+-- @param value (string): The display text of the selected option.
+-- @param data (any): The data associated with the selected option.
+function COMBOBOX:OnSelect(index, value, data)
     -- For override
 end
 
-function PANEL:OnMenuOpened(menu)
+--- Called when the dropdown menu is opened. Override this for custom behavior.
+-- @param menu (Panel): The menu panel that was opened.
+function COMBOBOX:OnMenuOpened(menu)
     -- For override
 end
 
-function PANEL:AddSpacer()
+--- Adds a visual spacer after the last added choice.
+function COMBOBOX:AddSpacer()
     self.Spacers[#self.Choices] = true
 end
 
-function PANEL:AddChoice(value, data, select, icon)
-    local index = table.insert(self.Choices, value)
-    if data then self.Data[index] = data end
-    if icon then self.ChoiceIcons[index] = icon end
-    if select then self:ChooseOption(value, index) end
+--- Adds a choice to the ComboBox.
+-- @param value (string): The display text for the choice.
+-- @param data (any): Optional data to associate with the choice.
+-- @param select (boolean): Whether to select this choice immediately.
+-- @param icon (string): Optional icon material name for the choice.
+-- @return (number): The index of the added choice.
+function COMBOBOX:AddChoice(value, data, select, icon)
+    local index = DTable:Add(self.Choices, value)
+    if data then
+        self.Data[index] = data
+    end
+
+    if icon then
+        self.ChoiceIcons[index] = icon
+    end
+
+    if select then
+        self:ChooseOption(value, index)
+    end
     return index
 end
 
-function PANEL:RemoveChoice(index)
-    if (not isnumber(index)) then return end
+--- Removes a choice from the ComboBox by index.
+-- @param index (number): The index of the choice to remove.
+-- @return (string, any): The removed choice's text and data.
+function COMBOBOX:RemoveChoice(index)
+    if not _isnumber(index) then
+        return
+    end
 
-    local text = table.remove(self.Choices, index)
-    local data = table.remove(self.Data, index)
+    local text = DTable:Remove(self.Choices, index)
+    local data = DTable:Remove(self.Data, index)
     return text, data
 end
 
-function PANEL:IsMenuOpen()
-    return IsValid(self.Menu) && self.Menu:IsVisible()
+--- Checks if the dropdown menu is currently open.
+-- @return (boolean): True if the menu is open and visible.
+function COMBOBOX:IsMenuOpen()
+    return _IsValid(self.Menu) and self.Menu:IsVisible()
 end
 
-function PANEL:OpenMenu(pControlOpener)
-    if (pControlOpener && pControlOpener == self.TextEntry) then
+--- Opens the dropdown menu with all available choices.
+-- @param pControlOpener (Panel): The control that triggered the opening (internal use).
+function COMBOBOX:OpenMenu(pControlOpener)
+    if pControlOpener and pControlOpener == self.TextEntry then
         return
     end
 
-    -- Don't do anything if there aren't any options..
-    if (#self.Choices == 0) then
+    if #self.Choices == 0 then
         return
     end
 
-    -- If the menu still exists and hasn't been deleted
-    -- then just close it and don't open a new one.
+    -- Close any other open ComboBox
+    if (_IsValid(ActiveComboBox) and ActiveComboBox ~= self) then
+        ActiveComboBox:CloseMenu()
+    end
+
     self:CloseMenu()
+    ActiveComboBox = self
 
-    -- If we have a modal parent at some level, we gotta parent to
-    -- that or our menu items are not gonna be selectable
+    -- Find modal parent if exists
     local parent = self
-    while (IsValid(parent) && not parent:IsModal()) do
+    while _IsValid(parent) and not parent:IsModal() do
         parent = parent:GetParent()
     end
 
-    if (not IsValid(parent)) then
+    if not _IsValid(parent) then
         parent = self
     end
 
-    self.Menu = base:UIContextMenu(parent)
+    self.Menu = DBase:UIContextMenu(parent)
     self.iconAngle = 180
+    
+    -- Reset icon angle when menu is removed
+    self.Menu.OnRemove = function()
+        self.iconAngle = 0
+        if ActiveComboBox == self then
+            ActiveComboBox = nil
+        end
+    end
 
     if self:GetSortItems() then
         local sorted = {}
-        for k, v in pairs(self.Choices) do
-            local val = tostring(v)
-            if (string.len(val) > 1 && not tonumber(val) && val:StartsWith('#')) then
+        for k, v in _pairs(self.Choices) do
+            local val = _tostring(v)
+            if (_stringlen(val) > 1 and not _tonumber(val) and val:StartsWith('#')) then
                 val = language.GetPhrase(val:sub(2))
             end
-            table.insert(sorted, { id = k, data = v, label = val })
+
+            DTable:Add(sorted, {
+                id = k,
+                data = v,
+                label = val
+            })
         end
 
         for k, v in SortedPairsByMemberValue(sorted, 'label') do
-            local option = self.Menu:Option(v.data, nil, self.ChoiceIcons[v.id] and self.ChoiceIcons[v.id] or nil, function()
+            self.Menu:Option(v.data, nil, self.ChoiceIcons[v.id], function()
                 self:ChooseOption(v.data, v.id)
             end)
 
             if self.Spacers[v.id] then
-                --[[ self.Menu:Spacer() ]]
+                -- Reserved for future spacer implementation
             end
         end
     else
-        for k, v in pairs(self.Choices) do
-            local option = self.Menu:Option(v, nil, self.ChoiceIcons[k] and self.ChoiceIcons[k] or nil, function()
+        for k, v in _pairs(self.Choices) do
+            self.Menu:Option(v, nil, self.ChoiceIcons[k], function()
                 self:ChooseOption(v, k)
             end)
 
             if self.Spacers[k] then
-                --[[ self.Menu:Spacer() ]]
+                -- Reserved for future spacer implementation
             end
         end
     end
@@ -218,90 +283,99 @@ function PANEL:OpenMenu(pControlOpener)
     self:OnMenuOpened(self.Menu)
 end
 
-function PANEL:CloseMenu()
-    if IsValid(self.Menu) then self.Menu:Remove() end
+--- Closes the dropdown menu if it's open.
+function COMBOBOX:CloseMenu()
+    if _IsValid(self.Menu) then
+        self.Menu:Remove()
+    end
     self.Menu = nil
     self.iconAngle = 0
 end
 
--- This really should use a convar change hook
-function PANEL:CheckConVarChanges()
-    if (not self.m_strConVar) then return end
-
-    local strValue = GetConVarString(self.m_strConVar)
-    if (self.m_strConVarValue == strValue) then return end
-
-    self.m_strConVarValue = strValue
-    self:SetValue(self:GetOptionTextByData(self.m_strConVarValue))
-end
-
---- Sets the direction of the text in the combo box.
--- @param d: Text Direction.
-function PANEL:SetDirection(d)
+--- Sets the text alignment direction.
+-- @param d (boolean): True for left-aligned text with right icon, false for centered text.
+-- @return (Panel): Returns self for method chaining.
+function COMBOBOX:SetDirection(d)
     self.Direction = d
+    return self
 end
 
---- Sets the font for the text in the combo box.
--- @param font: The font to be used.
-function PANEL:SetFont(font)
+--- Sets the font for the ComboBox text.
+-- @param font (string): The font name to use.
+-- @return (Panel): Returns self for method chaining.
+function COMBOBOX:SetFont(font)
     self.setFont = font
+    return self
 end
 
-function PANEL:Think()
-    self:CheckConVarChanges()
-end
-
-function PANEL:SetValue(strValue)
+--- Sets the displayed value/text of the ComboBox.
+-- @param strValue (string): The text to display.
+function COMBOBOX:SetValue(strValue)
     self.text = strValue
 end
 
-function PANEL:DoClick()
-    if self:IsMenuOpen() then return self:CloseMenu() end
+--- Handles click events on the ComboBox.
+function COMBOBOX:DoClick()
+    if self:IsMenuOpen() then
+        return self:CloseMenu()
+    end
     self:OpenMenu()
 end
 
--- Turning off shadows
-function PANEL:DisableShadows(distance, noClip, iteration)
+--- Applies shadow effect to the ComboBox.
+-- @param distance (number): Shadow distance (default: 10).
+-- @param noClip (boolean): Whether to disable clipping (default: false).
+-- @param iteration (number): Number of shadow iterations (default: 5).
+-- @return (Panel): Returns self for method chaining.
+function COMBOBOX:DisableShadows(distance, noClip, iteration)
     self:ApplyShadow(distance or 10, noClip or false, iteration or 5)
     return self
 end
 
---- Draws a combo box.
-function PANEL:Paint(w, h)
+--- Renders the ComboBox with all visual elements.
+-- @param w (number): Width of the panel.
+-- @param h (number): Height of the panel.
+function COMBOBOX:Paint(w, h)
     self:ApplyAlpha(0.1, 155, false, false, self:IsMenuOpen(), 155)
 
-    self.hover = self:IsMenuOpen() and math.min((self.hover or 0) + 5, 100) or math.max((self.hover or 0) - 5, 0)
+    -- Smooth hover animation
+    self.hover = self:IsMenuOpen() and _min((self.hover or 0) + 5, 100) or _max((self.hover or 0) - 5, 0)
     local hover = self.hover / 100
 
-    utils:DrawRoundedBox(0, 0, w, h, base:Theme('decor_elements'))
-    utils:OutlinedRect(0, 0, w, h, base:Theme('frame'))
-    utils:OutlinedRect(0, 0, w, h, base:Theme('decor', hover * 100))
+    -- Draw background and borders
+    DUtils:DrawRoundedBox(0, 0, w, h, DBase:Theme('decor_elements'))
+    DUtils:OutlinedRect(0, 0, w, h, DBase:Theme('frame'))
+    DUtils:OutlinedRect(0, 0, w, h, DBase:Theme('decor', hover * 100))
 
+    -- Draw icon if set
     local x = 10
     if self.setIcon then
-        local iconSize = base:Scale(30)
-        utils:DrawIconOrMaterial(h * 0.5 - iconSize * 0.5, h * 0.5 - iconSize * 0.5, iconSize, self.setIcon, base:Theme('mat', 75 + self.alpha))
+        local iconSize = DBase:Scale(30)
+        DUtils:DrawIconOrMaterial(h * 0.5 - iconSize * 0.5, h * 0.5 - iconSize * 0.5, iconSize, self.setIcon, DBase:Theme('mat', 75 + self.alpha))
         x = x + 20
     end
 
+    -- Draw dropdown arrow with smooth rotation
     if self.Direction then
         local sz = 16
-        self.deg = Lerp(FrameTime() * 15, self.deg or 0, self.iconAngle)
-        utils:DrawIconRotated(w - sz - 5, h / 2, sz, sz, self.deg, DanLib.Config.Materials['Arrow'], base:Theme('decor', 100 + self.alpha))
+        self.deg = _Lerp(_FrameTime() * 15, self.deg or 0, self.iconAngle)
+        DUtils:DrawIconRotated(w - sz - 5, h / 2, sz, sz, self.deg, DanLib.Config.Materials['Arrow'], DBase:Theme('decor', 100 + self.alpha))
     end
 
-    local text = self.text or base:L('#no.data')
+    -- Draw text
+    local text = self.text or DBase:L('#no.data')
     local font = self.setFont or 'danlib_font_18'
-    draw.SimpleText(text, font, self.Direction and x or w * 0.5, h * 0.5, base:Theme('text', 100 + self.alpha), self.Direction and TEXT_ALIGN_LEFT or TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+    _SimpleText(text, font, self.Direction and x or w * 0.5, h * 0.5, DBase:Theme('text', 100 + self.alpha), self.Direction and TEXT_ALIGN_LEFT or TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
 end
 
-derma.DefineControl('DanLib.UI.ComboBox', '', PANEL, 'DButton')
+COMBOBOX:SetBase('DButton')
+COMBOBOX:Register('DanLib.UI.ComboBox')
 
-
-function base.CreateUIComboBox(parent)
+--- Creates a new ComboBox instance.
+-- @param parent (Panel): The parent panel to attach the ComboBox to.
+-- @return (Panel): The created ComboBox panel.
+function DBase.CreateUIComboBox(parent)
     parent = parent or nil
-
-    local ComboBox = DanLib.CustomUtils.Create(parent, 'DanLib.UI.ComboBox')
+    local ComboBox = DCustomUtils(parent, 'DanLib.UI.ComboBox')
     return ComboBox
 end
-
